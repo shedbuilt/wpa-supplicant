e6b12a1a0b8285b1ea80cb7b2c07caa5916166da
Auston Stewart <auston@shedbuilt.net>
2018-04-26 16:56:23 -1000

    Fixing bad source MD5

--- postinstall.sh
+++ postinstall.sh
@@ -4,4 +4,5 @@ if [ $? -eq 0 ]; then
     systemctl restart wpa_supplicant
 else
     systemctl enable wpa_supplicant
+    echo "NOTE: You must restart your device in order for WPA Supplicant to function correctly."
 fi
