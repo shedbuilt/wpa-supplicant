3571188bb2140e68982037b1ceafa241f64fd1a2
Auston Stewart <auston@shedbuilt.net>
2018-06-06 09:09:09 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-patch -p1 -i "${SHED_PATCHDIR}/wpa_supplicant-2.6-upstream_fixes-1.patch" &&
+patch -p1 -i "${SHED_PKG_PATCH_DIR}/wpa_supplicant-2.6-upstream_fixes-1.patch" &&
 cd wpa_supplicant &&
-cp "${SHED_CONTRIBDIR}/default.config" .config &&
-make -j $SHED_NUMJOBS BINDIR=/sbin LIBDIR=/lib &&
-install -v -dm755 "${SHED_FAKEROOT}/sbin" &&
-install -v -m755 wpa_{cli,passphrase,supplicant} "${SHED_FAKEROOT}/sbin" &&
-install -v -dm755 "${SHED_FAKEROOT}/usr/share/man/man5" &&
-install -v -m644 doc/docbook/wpa_supplicant.conf.5 "${SHED_FAKEROOT}/usr/share/man/man5" &&
-install -v -dm755 "${SHED_FAKEROOT}/usr/share/man/man8" &&
-install -v -m644 doc/docbook/wpa_{cli,passphrase,supplicant}.8 "${SHED_FAKEROOT}/usr/share/man/man8" &&
-install -v -dm755 "${SHED_FAKEROOT}/lib/systemd/system" &&
-install -v -m644 systemd/*.service "${SHED_FAKEROOT}/lib/systemd/system" &&
-install -v -dm755 "${SHED_FAKEROOT}/usr/share/dbus-1/system-services" &&
+cp "${SHED_PKG_CONTRIB_DIR}/default.config" .config &&
+make -j $SHED_NUM_JOBS BINDIR=/sbin LIBDIR=/lib &&
+install -v -dm755 "${SHED_FAKE_ROOT}/sbin" &&
+install -v -m755 wpa_{cli,passphrase,supplicant} "${SHED_FAKE_ROOT}/sbin" &&
+install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/man/man5" &&
+install -v -m644 doc/docbook/wpa_supplicant.conf.5 "${SHED_FAKE_ROOT}/usr/share/man/man5" &&
+install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/man/man8" &&
+install -v -m644 doc/docbook/wpa_{cli,passphrase,supplicant}.8 "${SHED_FAKE_ROOT}/usr/share/man/man8" &&
+install -v -dm755 "${SHED_FAKE_ROOT}/lib/systemd/system" &&
+install -v -m644 systemd/*.service "${SHED_FAKE_ROOT}/lib/systemd/system" &&
+install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/dbus-1/system-services" &&
 install -v -m644 dbus/fi.{epitest.hostap.WPASupplicant,w1.wpa_supplicant1}.service \
-                 "${SHED_FAKEROOT}/usr/share/dbus-1/system-services" &&
-install -v -dm755 "${SHED_FAKEROOT}/etc/dbus-1/system.d" &&
+                 "${SHED_FAKE_ROOT}/usr/share/dbus-1/system-services" &&
+install -v -dm755 "${SHED_FAKE_ROOT}/etc/dbus-1/system.d" &&
 install -v -m644 dbus/dbus-wpa_supplicant.conf \
-                 "${SHED_FAKEROOT}/etc/dbus-1/system.d/wpa_supplicant.conf"
+                 "${SHED_FAKE_ROOT}/etc/dbus-1/system.d/wpa_supplicant.conf"
